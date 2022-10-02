454a18a5b4f27a91e0c15c0ed68204d3b5462e7a
Vova <vvvova24@gamil.com>
2022-10-02 13:28:57 +0600

    Last

--- Hw_2_5.py
+++ Hw_2_5.py
@@ -1,9 +1,24 @@
+#Реализуйте алгоритм перемешивания списка.
+
 import random
 
+def rand(first, second):
+    return random.randint(first, second)
+
 mass = []
+mass0 = []
 num = 10
+
 for i in range(num):
     mass.append(i)
 
-for i in range(len(mass)):
-    mass[i]
+ind = 0
+sch = 0
+while sch == len(mass):
+    ind = rand(0, len(mass))
+    if ind not in mass0:
+        mass0.append(ind)
+        sch = sch + 1
+
+for i in range(0, len(mass0)):
+    print(i)
